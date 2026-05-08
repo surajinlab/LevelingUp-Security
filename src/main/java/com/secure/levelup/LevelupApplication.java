30e73ad0d57237f51279e246fcd1fc8e42d03cf9
surajinlab <suraj.in.lab@gmail.com>
2026-05-08 09:52:03 +0530

    first commiting

---
+++ src/main/java/com/secure/levelup/LevelupApplication.java
@@ -0,0 +1,14 @@
+package com.secure.levelup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelupApplication.class, args);
+		System.out.println("........LevelUp Security by Java.........");
+	}
+
+}
